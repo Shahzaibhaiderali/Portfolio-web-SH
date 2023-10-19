0162149614948ffb4cc0804c9c4fb73db1637cd2
Shahzaibhaiderali <shahzaibhaiderali1@gmail.com>
2023-10-19 19:33:39 +0500

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const navMenu = document.querySelector('ul.nav');
-const menuToggle = document.querySelector('.menu-toggle');
-
-// Add a click event listener to the menu toggle icon
-menuToggle.addEventListener('click', () => {
-  // Toggle the 'active' class on the navigation menu
-  navMenu.classList.toggle('active');
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const nav = document.querySelector('ul.nav');
+    nav.classList.toggle('active');
 });
