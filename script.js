9612830fc944f1b658fb3c2c87bcf2bf8150add3
Shahzaibhaiderali <shahzaibhaiderali1@gmail.com>
2023-10-24 04:13:01 +0500

    first commit

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
     const nav = document.querySelector('ul.nav');
     nav.classList.toggle('active');
 });
+
+
+ // JavaScript for smooth scrolling to sections
+ function scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
