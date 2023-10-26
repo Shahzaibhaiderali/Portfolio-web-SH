95e043dd6eccb4218df215f65b25eb4ee16aeabb
Shahzaibhaiderali <shahzaibhaiderali1@gmail.com>
2023-10-26 18:15:14 +0500

    first commit

--- script.js
+++ script.js
@@ -36,5 +36,45 @@ window.addEventListener('resize', checkSkills);
 // Initial check
 checkSkills();
 
+const sections = document.querySelectorAll("section");
+
+        // Callback function to handle intersection
+        function handleIntersection(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // Add a CSS class to trigger the animation
+                    entry.target.classList.add("animate");
+                }
+            });
+        }
+
+        // Create an Intersection Observer
+        const observer = new IntersectionObserver(handleIntersection, {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.2, // Adjust this threshold as needed
+        });
+
+        // Observe all sections
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+
+
+        document.addEventListener("DOMContentLoaded", function () {
+          const contactForm = document.getElementById("contact-form");
+      
+          contactForm.addEventListener("submit", function (e) {
+              e.preventDefault();
+      
+              // You can add form validation and submission logic here
+              // For example, you can use the Fetch API to send the form data to your server.
+              
+              // For a basic example, you can simply clear the form fields:
+              contactForm.reset();
+              alert("Form submitted successfully!"); // You can replace this with your own logic
+          });
+      });
+      
 
 
