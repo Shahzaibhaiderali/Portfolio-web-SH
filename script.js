7a6413e6037993c165189378ef662f96c3d61771
Shahzaibhaiderali <shahzaibhaiderali1@gmail.com>
2023-10-27 04:20:52 +0500

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
+document.querySelector('.menu-toggle').addEventListener('click', function () {
     const nav = document.querySelector('ul.nav');
     nav.classList.toggle('active');
 });
 
 
- // JavaScript for smooth scrolling to sections
- function scrollToSection(sectionId) {
+// JavaScript for smooth scrolling to sections
+function scrollToSection(sectionId) {
     const targetSection = document.getElementById(sectionId);
     if (targetSection) {
         window.scrollTo({
@@ -18,16 +18,16 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
 const skills = document.querySelectorAll('.skill');
 
 function checkSkills() {
-  skills.forEach((skill) => {
-    const skillPosition = skill.getBoundingClientRect().top;
-    const scrollPosition = window.innerHeight / 1.3;
+    skills.forEach((skill) => {
+        const skillPosition = skill.getBoundingClientRect().top;
+        const scrollPosition = window.innerHeight / 1.3;
 
-    if (skillPosition < scrollPosition) {
-      const rating = skill.querySelector('.rating');
-      const percent = rating.getAttribute('data-percent');
-      rating.style.width = percent + '%';
-    }
-  });
+        if (skillPosition < scrollPosition) {
+            const rating = skill.querySelector('.rating');
+            const percent = rating.getAttribute('data-percent');
+            rating.style.width = percent + '%';
+        }
+    });
 }
 
 window.addEventListener('scroll', checkSkills);
@@ -38,43 +38,43 @@ checkSkills();
 
 const sections = document.querySelectorAll("section");
 
-        // Callback function to handle intersection
-        function handleIntersection(entries, observer) {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    // Add a CSS class to trigger the animation
-                    entry.target.classList.add("animate");
-                }
-            });
+// Callback function to handle intersection
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Add a CSS class to trigger the animation
+            entry.target.classList.add("animate");
         }
+    });
+}
 
-        // Create an Intersection Observer
-        const observer = new IntersectionObserver(handleIntersection, {
-            root: null,
-            rootMargin: "0px",
-            threshold: 0.2, // Adjust this threshold as needed
-        });
+// Create an Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2, // Adjust this threshold as needed
+});
 
-        // Observe all sections
-        sections.forEach(section => {
-            observer.observe(section);
-        });
+// Observe all sections
+sections.forEach(section => {
+    observer.observe(section);
+});
 
 
-        document.addEventListener("DOMContentLoaded", function () {
-          const contactForm = document.getElementById("contact-form");
-      
-          contactForm.addEventListener("submit", function (e) {
-              e.preventDefault();
-      
-              // You can add form validation and submission logic here
-              // For example, you can use the Fetch API to send the form data to your server.
-              
-              // For a basic example, you can simply clear the form fields:
-              contactForm.reset();
-              alert("Form submitted successfully!"); // You can replace this with your own logic
-          });
-      });
-      
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // You can add form validation and submission logic here
+        // For example, you can use the Fetch API to send the form data to your server.
+
+        // For a basic example, you can simply clear the form fields:
+        contactForm.reset();
+        alert("Form submitted successfully!"); // You can replace this with your own logic
+    });
+});
+
 
 
